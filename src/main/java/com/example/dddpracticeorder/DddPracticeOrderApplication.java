5717ef9356f3ef02cbaefde033dc64a36b1e3534
renechoi <115696395+renechoi@users.noreply.github.com>
2023-06-22 16:33:10 +0900

    feat: initial commit - project setup

---
+++ src/main/java/com/example/dddpracticeorder/DddPracticeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddpracticeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddPracticeOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddPracticeOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dddpracticeorder/DddPracticeOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddpracticeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddPracticeOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
