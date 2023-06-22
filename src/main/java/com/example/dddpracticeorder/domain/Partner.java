1c9c9f2ec3293d2b627425f9ee6cc97a3359fe43
renechoi <115696395+renechoi@users.noreply.github.com>
2023-06-22 16:34:19 +0900

    feat: implement partner entity

---
+++ src/main/java/com/example/dddpracticeorder/domain/Partner.java
@@ -0,0 +1,68 @@
+package com.example.dddpracticeorder.domain;
+
+import static org.apache.commons.lang3.StringUtils.*;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.example.dddpracticeorder.common.exception.InvalidParamException;
+import com.example.dddpracticeorder.common.util.TokenGenerator;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "partners")
+public class Partner extends AbstractEntity {
+	private static final String PREFIX_PARTNER = "ptn_";
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String partnerToken;
+	private String partnerName;
+	private String businessNo;
+	private String email;
+
+	@Enumerated(EnumType.STRING)
+	private Status status;
+
+	@Getter
+	@RequiredArgsConstructor
+	public enum Status {
+		ENABLE("활성화"), DISABLE("비활성화");
+		private final String description;
+	}
+
+	@Builder
+	public Partner(String partnerName, String businessNo, String email) {
+		if (isEmpty(partnerName)) throw new InvalidParamException("empty partnerName");
+		if (isEmpty(businessNo)) throw new InvalidParamException("empty businessNo");
+		if (isEmpty(email)) throw new InvalidParamException("empty email");
+
+		this.partnerToken = TokenGenerator.randomCharacterWithPrefix(PREFIX_PARTNER);
+		this.partnerName = partnerName;
+		this.businessNo = businessNo;
+		this.email = email;
+		this.status = Status.ENABLE;
+	}
+	public void enable() {
+		this.status = Status.ENABLE;
+	}
+
+	public void disable() {
+		this.status = Status.DISABLE;
+	}
+
+}
